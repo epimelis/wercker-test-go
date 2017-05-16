5bf442df2814b5f05d46ede168ab4ef861c15c7a
A Yong <andraceyong@gmail.com>
2017-05-16 12:16:52 -0400

    12:16

--- main.go
+++ main.go
@@ -2,35 +2,35 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
-        "fmt"
-        "os"
-        "html/template"
+	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {  
-        t, err := template.ParseFiles("templates/page.html")
-        if err != nil {
-             panic(err)
-        }
-        t.Execute(w, nil)
+func handler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/page.html")
+	if err != nil {
+		panic(err)
+	}
+	t.Execute(w, nil)
 }
 
 func CityHandler(res http.ResponseWriter, req *http.Request) {
-	data, _ := json.Marshal("{'cities':'San Francisco, Amsterdam, Berlin, New York','Tokyo'}")
+	data, _ := json.Marshal("{'cities':'Toronto, Montreal'}")
 	res.Header().Set("Content-Type", "application/json; charset=utf-8")
 	res.Write(data)
 }
 
 func main() {
-        port := os.Getenv("WEB_SERVER_PORT")
-        if port=="" {
-            port = ":8000"
-        }
-        fmt.Printf("Listening on port %s!\n",port)
+	port := os.Getenv("WEB_SERVER_PORT")
+	if port == "" {
+		port = ":8000"
+	}
+	fmt.Printf("Listening on port %s!\n", port)
 	http.HandleFunc("/cities.json", CityHandler)
-        http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
