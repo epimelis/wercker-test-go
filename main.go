5573226a99ca2e70c9ccc83aaf7aa15861f74c80
Epimelis2 <epimelis2@gmail.com>
2017-10-03 16:45:49 -0400

    commit

--- main.go
+++ main.go
@@ -10,7 +10,9 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("templates/page.html")
+	//t, err := template.ParseFiles("templates/page.html")
+	//w.Header().Set("Access-Control-Allow-Origin", "*")
+	t, err :=template.ParseFiles("charts/csco.html")
 	if err != nil {
 		panic(err)
 	}
@@ -23,14 +25,27 @@ func CityHandler(res http.ResponseWriter, req *http.Request) {
 	res.Write(data)
 }
 
+
+func handler_charts2(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	t, err := template.ParseFiles("charts/csco-daily.original.csv")
+	if err != nil {
+		panic(err)
+	}
+	t.Execute(w, nil)
+}
+
+
 func main() {
 	port := os.Getenv("WEB_SERVER_PORT")
 	if port == "" {
 		port = ":8000"
 	}
 	fmt.Printf("Listening on port %s!\n", port)
-	http.HandleFunc("/cities.json", CityHandler)
+
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/cities.json", CityHandler)
+	http.HandleFunc("/charts2/csco-daily.csv", handler_charts2)
 
 	fs := http.FileServer(http.Dir("charts"))
 	http.Handle("/charts/", http.StripPrefix("/charts/", fs))
